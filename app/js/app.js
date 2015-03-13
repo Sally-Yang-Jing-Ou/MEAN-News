2de63a22a675fcdc32d8ed3001ae4a05de6d7393
Sally-Yang-Jing-Ou <sally_auyoung@hotmail.com>
2015-03-12 23:58:20 -0400

    Static info. Enabled upvoting, need more style

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,24 @@
 var app = angular.module('MEAN-News', []);
 
-app.controller('MainController', function($scope){
-	$scope.test = 'Hello World!';
-});
+app.controller('MainController', ['$scope', function($scope){
+	$scope.posts = [{title: 'post 1', upvotes: 5},
+  					{title: 'post 2', upvotes: 2},
+  				    {title: 'post 3', upvotes: 15},
+  					{title: 'post 4', upvotes: 9},
+  					{title: 'post 5', upvotes: 4}];
+
+    //prevent blank posts
+    if(!$scope.title || $scope.title === '') {return;}
+  	$scope.addPosts = function(){
+  		$scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});
+  		$scope.title = '';
+  		$scope.link = '';
+  	};
+
+  	$scope.upvoting = function(post){
+  		post.upvotes += 1;
+  	};
+
+
+}]);
 
